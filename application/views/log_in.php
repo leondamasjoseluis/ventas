0be1f4a8fbab911e512308c6b888872f540c5670
LeonJose <joseluisleondamas@gmail.com>
2016-10-06 02:09:25 -0700

    Agregando Codeigniter

---
+++ application/views/log_in.php
@@ -0,0 +1,23 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Inisiar Secion</title>
+	<style>
+		body{
+			font-family: Arial;
+			background-color: orange;
+		}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Hol a</h1>
+</div>
+</body>
+</html>
